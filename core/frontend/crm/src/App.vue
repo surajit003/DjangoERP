18945df4744a5d6ae454625396cdcbbc1b817e73
Surajit Das <67738912+surajit003@users.noreply.github.com>
2023-02-25 16:10:48 +0300

    Ft/crm app (#1)
    Initial set up for vue scaffolding for crm

---
+++ core/frontend/crm/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Navbar/>
+    <section class="section">
+      <router-view/>
+    </section>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/layout/Navbar'
+import axios from "axios";
+
+export default {
+  name: "App",
+  components: {Navbar},
+  beforeCreate() {
+    this.$store.commit('initializeStore')
+    if (this.$store.state.token) {
+      axios.defaults.headers.common['Authorization'] = "Token " + this.$store.state.token
+    } else {
+      axios.defaults.headers.common['Authorization'] = ""
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "../node_modules/bulma";
+</style>
---
+++ core/frontend/crm/src/views/dashboard/MyAccountView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h1 class="title">My account</h1>
+      </div>
+
+      <div class="column is-12">
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyAccountView"
+}
+</script>
