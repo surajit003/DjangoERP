fc7f861eb3fa068212c7a28dd378c41624845b2c
Surajit Das <surajit.das0320@gmail.com>
2023-02-25 13:16:27 +0300

    add vue js default app

---
+++ core/frontend/crm/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld :msg="message" :dummy="dummy"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data(){
+    return {
+      message: 'Hello World!', dummy: 'Hey Dummy'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
