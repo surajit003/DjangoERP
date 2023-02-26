70f518b7b42137c4601542c365915c3bbf430745
Surajit Das <surajit.das0320@gmail.com>
2023-02-26 22:47:25 +0300

    fix return type hints

--- core/apps/crm/handlers/factories.py
+++ core/apps/crm/handlers/factories.py
@@ -1,11 +1,12 @@
 from abc import abstractmethod, ABC
 
+from core.apps.crm.repo.base import AbstractLeadRepository
 from core.apps.crm.repo.lead import LeadRepository
 
 
 class AbstractUseCaseFactory(ABC):
     @abstractmethod
-    def get_repo(self):
+    def get_repo(self) -> AbstractLeadRepository:
         ...
 
 
@@ -14,5 +15,5 @@ class InternalUseCaseFactory(AbstractUseCaseFactory):
     Use this factory when you want to save the data in DjangoERP
     """
 
-    def get_repo(self):
+    def get_repo(self) -> LeadRepository:
         return LeadRepository()
