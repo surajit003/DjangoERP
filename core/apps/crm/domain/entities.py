9f9f73133eb1c8435f3a1cf4512d159e5e4388ec
Surajit Das <surajit.das0320@gmail.com>
2023-02-26 16:19:48 +0300

    custom validation function for phone

--- core/apps/crm/domain/entities.py
+++ core/apps/crm/domain/entities.py
@@ -1,6 +1,7 @@
+import re
 from enum import Enum
 
-from pydantic import EmailStr, AnyHttpUrl
+from pydantic import EmailStr, AnyHttpUrl, validator
 
 from core.apps.common.entities import Entity
 
@@ -29,3 +30,12 @@ class Lead(Entity):
     estimated_value: int
     status: LeadStatus
     priority: PriorityStatus
+
+    @validator("phone")
+    def phone_validation(cls, v):
+        # For reference - https://stackoverflow.com/questions/70414211/pydantic-custom-data-type-for-phone-number
+        # -value-error-missing
+        regex = r"^(\+)[1-9][0-9\-\(\)\.]{9,15}$"
+        if v and not re.search(regex, v, re.I):
+            raise ValueError("Invalid Phone Number.")
+        return v
