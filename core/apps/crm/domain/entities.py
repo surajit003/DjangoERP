f757792f5f6d606423f66e0b09078a45043b9042
Surajit Das <surajit.das0320@gmail.com>
2023-02-26 15:59:33 +0300

    base entity for lead

--- core/apps/crm/domain/entities.py
+++ core/apps/crm/domain/entities.py
@@ -1,5 +1,9 @@
 from enum import Enum
 
+from pydantic import EmailStr, AnyHttpUrl
+
+from core.apps.common.entities import Entity
+
 
 class LeadStatus(str, Enum):
     NEW = "new"
@@ -7,3 +11,21 @@ class LeadStatus(str, Enum):
     IN_PROGRESS = "inprogress"
     LOST = "lost"
     WON = "won"
+
+
+class PriorityStatus(str, Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+
+class Lead(Entity):
+    company: str
+    contact_person: str
+    email: EmailStr
+    phone: str
+    website: AnyHttpUrl
+    confidence: int
+    estimated_value: int
+    status: LeadStatus
+    priority: PriorityStatus
