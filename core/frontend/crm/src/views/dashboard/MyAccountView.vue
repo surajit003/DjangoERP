6fabbbf0b368cf7b2ee4c5c5f9cade99b40d887e
Surajit Das <surajit.das0320@gmail.com>
2023-02-26 13:56:58 +0300

    add logout functionality

--- core/frontend/crm/src/views/dashboard/MyAccountView.vue
+++ core/frontend/crm/src/views/dashboard/MyAccountView.vue
@@ -6,12 +6,33 @@
       </div>
 
       <div class="column is-12">
+        <div class="buttons">
+          <button @click="logout()" class="button is-danger">Log out</button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
-  name: "MyAccountView"
+  name: "MyAccountView",
+  methods: {
+    async logout() {
+     await axios
+          .post('/api/v1/token/logout')
+          .then(response => {
+            console.log(response);
+            this.$store.commit('removeToken')
+            axios.defaults.headers.common['Authorization'] = ''
+            localStorage.removeItem('token')
+            this.$router.push('/')
+          })
+          .catch(errors =>{
+            console.log(JSON.stringify(errors))
+          })
+    }
+  }
 }
 </script>
