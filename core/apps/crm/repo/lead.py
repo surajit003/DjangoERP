8c5d6b178b6dff313e01e173de3d9db0d7e10d0c
Surajit Das <surajit.das0320@gmail.com>
2023-02-26 18:26:47 +0300

    modify exception for lead

--- core/apps/crm/repo/lead.py
+++ core/apps/crm/repo/lead.py
@@ -1,12 +1,18 @@
+from django.db import IntegrityError
+
 from core.apps.crm.domain.entities import LeadEntity
 from core.apps.crm.models.lead import Lead
 from core.apps.crm.repo.base import AbstractLeadRepository
+from core.apps.crm.repo.exceptions import LeadExistException
 
 
 class LeadRepository(AbstractLeadRepository):
     def save(self, obj: LeadEntity):
-        lead_obj = Lead(**obj.dict())
-        return lead_obj.save()
+        try:
+            lead_obj = Lead(**obj.dict())
+            return lead_obj.save()
+        except IntegrityError as exc:
+            raise LeadExistException(exc)
 
     def update(self, obj: LeadEntity):
         pass
