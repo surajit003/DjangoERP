ae367b80854915297019af0eba0dce610b7c6a41
Surajit Das <67738912+surajit003@users.noreply.github.com>
2023-02-26 14:30:31 +0300

    Feature: Authentication Functionality (#2)
    Add Authentication functionality using djoser

--- DjangoERP/settings.py
+++ DjangoERP/settings.py
@@ -28,50 +28,55 @@ DEBUG = config("DEBUG_MODE", default=False)
 
 ALLOWED_HOSTS = []
 
-
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:8080",
+]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "corsheaders",
+    "djoser",
     "core.apps.crm",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'DjangoERP.urls'
+ROOT_URLCONF = "DjangoERP.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'DjangoERP.wsgi.application'
+WSGI_APPLICATION = "DjangoERP.wsgi.application"
 
 
 # Database
@@ -94,16 +99,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -111,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -123,9 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
--- core/frontend/crm/src/views/LoginView.vue
+++ core/frontend/crm/src/views/LoginView.vue
@@ -4,21 +4,23 @@
       <div class="column is-4 is-offset-4">
         <h1 class="title">Log in</h1>
 
-        <form>
+        <form @submit.prevent="loginForm">
           <div class="field">
             <label>Email</label>
             <div class="control">
-              <input type="email" name="email" class="input">
+              <input type="email" name="email" class="input" v-model="username">
             </div>
           </div>
 
           <div class="field">
             <label>Password</label>
             <div class="control">
-              <input type="password" name="password" class="input">
+              <input type="password" name="password" class="input" v-model="password">
             </div>
           </div>
-
+          <div class="notification is-danger" v-if="errors.length">
+            <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+          </div>
           <div class="field">
             <div class="control">
               <button class="button is-success">Submit</button>
@@ -30,7 +32,58 @@
   </div>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
-  name: "LoginView"
+  name: "LoginView",
+  data() {
+    return {
+      username: "",
+      password: "",
+      errors: [],
+    }
+  },
+  methods: {
+    loginForm() {
+      this.errors = []
+      if (this.username === '') {
+        this.errors.push("Email cannot be empty")
+      }
+
+      if (this.password === '') {
+        this.errors.push("Password cannot be empty")
+      }
+
+      if (!this.errors.length) {
+        this.$store.commit('setIsLoading', true)
+        axios.defaults.headers.common['Authorization'] = ''
+        localStorage.removeItem('token')
+
+        const loginFormData = {
+          username: this.username,
+          password: this.password
+        }
+
+        axios
+            .post('/api/v1/token/login/', loginFormData)
+            .then(response => {
+              const token = response.data.auth_token
+              this.$store.commit('setToken', token)
+              axios.defaults.headers.common['Authorization'] = 'Token ' + token
+              localStorage.setItem('token', token)
+              this.$router.push('/dashboard/my-account')
+            })
+            .catch(errors => {
+              if (errors.response) {
+                for (const property in errors.response.data) {
+                  this.errors.push(`${errors.response.data[property]}`)
+                }
+              } else if (errors.message) {
+                this.errors.push(errors.message);
+              }
+            })
+      }
+    }
+  }
 }
 </script>
