8a7381c74aecf063f630d13a8c40c234741987ba
Surajit Das <67738912+surajit003@users.noreply.github.com>
2023-02-27 10:52:12 +0300

    modify exception handling in handlers (#11)

--- core/apps/crm/handlers/web.py
+++ core/apps/crm/handlers/web.py
@@ -1,7 +1,9 @@
+from pydantic import ValidationError
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from core.apps.crm.domain.exceptions import InvalidPhoneNumberException
 from core.apps.crm.handlers.factories import InternalUseCaseFactory
 from core.apps.crm.logic.lead import create_lead
 from core.apps.crm.repo.exceptions import LeadExistException
@@ -19,4 +21,19 @@ class LeadAPIView(APIView):
                 {"message": "Lead with that Email Exist"},
                 status=status.HTTP_400_BAD_REQUEST,
             )
+        except InvalidPhoneNumberException:
+            return Response(
+                {
+                    "message": "Invalid Phone Number",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        except ValidationError as exc:
+            return Response(
+                {
+                    "message": exc.errors(),
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
         return Response(lead.dict(), status=status.HTTP_201_CREATED)
