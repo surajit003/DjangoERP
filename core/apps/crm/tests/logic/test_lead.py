aa50867d406c7fdc476b1fe2f69b44fad2faf35f
Surajit Das <surajit.das0320@gmail.com>
2023-03-01 22:38:01 +0300

    add test for all leads

--- core/apps/crm/tests/logic/test_lead.py
+++ core/apps/crm/tests/logic/test_lead.py
@@ -1,6 +1,6 @@
 import pytest
 
-from core.apps.crm.logic.lead import create_lead
+from core.apps.crm.logic.lead import create_lead, list_all
 
 pytestmark = pytest.mark.django_db
 
@@ -21,3 +21,11 @@ def test_update_lead(lead_factory, lead_repo):
     assert lead
     assert lead.company == lead_data_dict["company"]
     assert lead.contact_person == lead_data_dict["contact_person"]
+
+
+def test_list_all(lead_factory, lead_repo):
+    lead_factory()
+    lead_factory()
+    leads = list_all(lead_repo)
+    assert leads
+    assert len(leads) == 2
