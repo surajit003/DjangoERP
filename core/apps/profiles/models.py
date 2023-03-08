80a329cfe2430f04a83a8ab270865d4101e8d7ed
OkechEdu <okechkazi@outlook.com>
2023-03-08 03:20:15 +0300

    Added the user profile model

--- core/apps/profiles/models.py
+++ core/apps/profiles/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+from core.apps.common.models import TimeStampedUUIDModel
+
+User = get_user_model()
+
+
+class Gender(models.TextChoices):
+    MALE = "Male", _("Male")
+    FEMALE = "Female", _("Female")
+    OTHER = "Other", _("Other")
+    
+    
+class UserProfile(TimeStampedUUIDModel):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    phone_number = PhoneNumberField(
+        verbose_name=_("Phone Number"), max_length=30, default="+254720270267"
+    )
+    about_me = models.TextField(
+        verbose_name=_("About me"), default="say something about yourself"
+    )
+    profile_photo = models.ImageField(
+        verbose_name=_("Profile Photo/Logo"), default="/profile_default.png"
+    )
+    gender = models.CharField(
+        verbose_name=_("Gender"),
+        choices=Gender.choices,
+        default=Gender.OTHER,
+        max_length=20,
+    )
+    country = CountryField(
+        verbose_name=_("Country"), default="KE", blank=False, null=False
+    )
+    city = models.CharField(
+        verbose_name=_("City/Town"),
+        max_length=180,
+        default="Nairobi",
+        blank=False,
+        null=False,
+    )
+    address_one = models.CharField(
+        verbose_name=_("Address Line One"),
+        max_length=250,
+        blank=False,
+        null=False,
+    )
+    address_two = models.CharField(
+        verbose_name=_("Address Line Two"),
+        max_length=250,
+        blank=True,
+        null=True,
+    )
+        
+    def __str__(self):
+        return f"{self.user.username}'s profile"
