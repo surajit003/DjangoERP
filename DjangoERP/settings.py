581797513002c63a0a581fdb5350575464d5fed6
Surajit Das <surajit.das0320@gmail.com>
2023-03-08 16:40:01 +0300

    minor fix

--- DjangoERP/settings.py
+++ DjangoERP/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = "DjangoERP.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config("DATABASE_NAME"),
-        "USER": config("DATABASE_USER"),
-        "PASSWORD": config("DATABASE_PASSWORD"),
-        "HOST": config("DATABASE_HOST"),
+        "NAME": config("DATABASE_NAME", default="postgres"),
+        "USER": config("DATABASE_USER", default="postgres"),
+        "PASSWORD": config("DATABASE_PASSWORD", default="postgres"),
+        "HOST": config("DATABASE_HOST", default="localhost"),
         "PORT": config("DATABASE_PORT", default=5432, cast=int),
     }
 }
