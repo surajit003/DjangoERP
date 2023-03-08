4b8b5c15c5a318d1b9fea5c4af10cb166e5c6c4a
OkechEdu <okechkazi@outlook.com>
2023-03-08 03:03:49 +0300

    Added media url and root settings

--- DjangoERP/settings.py
+++ DjangoERP/settings.py
@@ -139,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
